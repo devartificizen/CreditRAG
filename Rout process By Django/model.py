0501e609a53a0c0f83440950d696cf837f8d1902
Sayiqa <sayiqajabeen14@gmail.com>
2025-01-16 02:20:35 +0500

    Create model.py

---
+++ Rout process By Django/model.py
@@ -0,0 +1,10 @@
+om django.db import models
+
+# Create your models here.
+from django.db import models
+
+class DisputeRequest(models.Model):
+    payment_status = models.IntegerField()
+    account_status = models.CharField(max_length=50)
+    creditor_remark = models.TextField()
+    dispute_letter_generated = models.BooleanField(default=False)
